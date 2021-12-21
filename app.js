cd9cea21afa82c1da86856dadf6a237e26aa27ae
Juan Andres Valencia <juanandres2018marzo@gmail.com>
2021-12-21 14:19:06 -0500

    new version sprint 5

--- app.js
+++ app.js
@@ -1,22 +1,34 @@
-const express = require("express");
-const app = express();
-const path = require("path");
-const port = process.env.PORT || 3000;
+const express = require("express")
+const session = require('express-session')
+const path = require("path")
+const port = process.env.PORT || 5000
 const methodOverride = require('method-override')
+const cookies = require('cookie-parser')
+const userLoggedMiddleware = require('./src/middlewares/userLoggedMiddleware')
+const app = express();
 
 app.use(express.static(path.resolve(__dirname, "public")));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json())
 app.use(methodOverride('_method'))
+app.use(session({
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(cookies())
+app.use(userLoggedMiddleware)
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'src/views'))
 
 const mainRouter = require('./src/routes/main');
 const productsRouter = require('./src/routes/products')
+const usersRouter = require('./src/routes/users')
 
 app.use('/', mainRouter)
 app.use('/products', productsRouter)
+app.use('/users', usersRouter)
 
 
 app.listen(port, () => console.log("server running on port " + port));
---
+++ src/middlewares/userLoggedMiddleware.js
@@ -0,0 +1,21 @@
+const User = require('../models/User') 
+
+function userLoggedMiddleware(req, res, next) {
+ 
+  res.locals.isLogged = false
+
+  if (req.session && req.session.userLogged){
+    res.locals.isLogged =true
+     res.locals.userLogged = req.session.userLogged
+  }
+  let emailInCookie = req.cookies.userEmail;
+  let userFromCookie = User.findByField('email', emailInCookie);
+
+  if (userFromCookie) {
+     req.session.userLogged = userFromCookie;
+  }
+
+  next()
+}
+
+module.exports = userLoggedMiddleware
